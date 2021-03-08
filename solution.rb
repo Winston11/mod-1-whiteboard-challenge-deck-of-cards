e8e1c1b7e32808f3149e9fdf6c86b2c349a6a777
winston11 <winstonmcgriff27@gmail.com>
2021-03-08 09:37:07 -0500

    commit tree 39c3f97bea2de5094cc6c2ca358c462846de0486

--- solution.rb
+++ solution.rb
@@ -1,7 +1,28 @@
-class Deck
+class Deck 
+    attr_accessor :cards
 
+    def initialize
+        @ranks = ("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K")
+        @suits = ("Hearts", "Clubs", "Diamonds", "Spades")
+        @cards = []
+    
+        @ranks.each do |rank|
+          @suits.each do |suit|
+            @cards << Card.new(rank, suit)
+          end
+        end
+      end
 end
 
-class Card
-
+class Card 
+attr_reader :rank, :suit, :color
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+    if @rank == 'Clubs' || @rank == 'Spades'
+      @color = 'black'
+    else
+      @color = 'red'
+    end
+  end
 end
